91d79ddb3f7e043feacccdf65b272231e421d8f3
hang <72736705+hangtouchone@users.noreply.github.com>
2023-02-03 01:10:08 +0800

    Delete admin-ui directory

--- admin-ui/src/main/java/com/hang/people_management/service/OssService.java
+++
@@ -1,8 +0,0 @@
-package com.hang.people_management.service;
-
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface OssService {
-    String uploadFileAvatar(MultipartFile file);
-}
